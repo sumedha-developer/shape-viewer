895d6dc691c3545ef7ce9a3dbb2373542ce942be
dsvallala <sumedha.developer@gmail.com>
2024-12-10 17:52:17 -0500

    LeftMenu.jsx

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const LeftMenu = () => {
+const LeftMenu = ({ shapeFileName, onFileUpload }) => {
   return (
     <aside className="left-menu">
-      <button>Open Shape File</button>
+      <button>
+        {shapeFileName || (
+          <label>
+            Open Shape File
+            <input type="file" onChange={onFileUpload} hidden />
+          </label>
+        )}
+      </button>
     </aside>
   );
+};
+
+LeftMenu.propTypes = {
+  shapeFileName: PropTypes.string,
+  onFileUpload: PropTypes.func.isRequired,
 };
 
 export default LeftMenu;
